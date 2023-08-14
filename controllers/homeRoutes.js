65fd9b2da3c654eca19769a2353cffbad07bd803
Chris_Gruen <113554860+ChristyGruen@users.noreply.github.com>
2023-08-14 17:04:35 -0500

    all tips and one tip routes working on homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,34 +3,41 @@ const { Tip, Question, Answer, User } = require('../models/');
 
 // get all posts for homepage
 router.get('/', async (req, res) => {
-  console.log('made it this far')
+  
     try {
     const tipData = await Tip.findAll({
-      include: [User],
+      include: [{
+        model:User,
+        attributes:['userName'],},],
     });
 
     const tips = tipData.map((tippy) => tippy.get({ plain: true }));
     console.log(tips)
 
-    // res.render('all-tips', { tips });
+    res.render('homepage', { tips });
   } catch (err) {
     res.status(500).json(err);
   }
-
-  // try {
-  //   const postData = await Post.findAll({
-  //     include: [User],
-  //   });
-
-  //   const posts = postData.map((post) => post.get({ plain: true }));
-
-  //   res.render('all-posts', { posts });
-  // } catch (err) {
-  //   res.status(500).json(err);
-  // }
 });
 
 // // get single post
+router.get('/tip/:id', async (req, res) => {
+  
+  try {
+  const tipData = await Tip.findByPk(req.params.id,{
+    include: [{
+      model:User,
+      attributes:['userName'],},],
+  });
+
+  const oneTip = tipData.get({plain:true});
+  console.log(oneTip)
+
+  res.render('homepage', {oneTip});
+} catch (err) {
+  res.status(500).json(err);
+}
+});
 // router.get('/post/:id', async (req, res) => {
 //   try {
 //     const postData = await Post.findByPk(req.params.id, {
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,22 +1,22 @@
 //change if needed; from MVC mini project
-module.exports = {
-  format_date: date => {
-    return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
-  },
-  format_amount: (amount) => {
-    // format large numbers with commas
-    return parseInt(amount).toLocaleString();
-  },
-  get_emoji: () => {
-    const randomNum = Math.random();
+// module.exports = {
+//   format_date: date => {
+//     return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+//   },
+//   format_amount: (amount) => {
+//     // format large numbers with commas
+//     return parseInt(amount).toLocaleString();
+//   },
+//   get_emoji: () => {
+//     const randomNum = Math.random();
 
-    // Return a random emoji
-    if (randomNum > 0.7) {
-      return `<span for="img" aria-label="lightbulb">💡</span>`;
-    } else if (randomNum > 0.4) {
-      return `<span for="img" aria-label="laptop">💻</span>`;
-    } else {
-      return `<span for="img" aria-label="gear">⚙️</span>`;
-    }
-  },
-};
+//     // Return a random emoji
+//     if (randomNum > 0.7) {
+//       return `<span for="img" aria-label="lightbulb">💡</span>`;
+//     } else if (randomNum > 0.4) {
+//       return `<span for="img" aria-label="laptop">💻</span>`;
+//     } else {
+//       return `<span for="img" aria-label="gear">⚙️</span>`;
+//     }
+//   },
+// };
