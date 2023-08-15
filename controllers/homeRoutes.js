45fbd37fbbe248b5ecb6a19d087a04a787b0cd7b
J-Stiehl <salsajackie@yahoo.com>
2023-08-15 09:40:26 -0500

    updated questions.handlebars and css

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -6,19 +6,19 @@ router.get("/", (req, res) => {
   res.render("homepage");
 });
 
-router.get("/", async (req, res) => {
-  try {
-    const postData = await Post.findAll({
-      include: [User],
-    });
+// router.get("/", async (req, res) => {
+//   try {
+//     const postData = await Post.findAll({
+//       include: [User],
+//     });
 
-    const posts = postData.map((post) => post.get({ plain: true }));
+//     const posts = postData.map((post) => post.get({ plain: true }));
 
-    res.render("all-posts", { posts });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+//     res.render("all-posts", { posts });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
 // get single post
 router.get("/post/:id", async (req, res) => {
