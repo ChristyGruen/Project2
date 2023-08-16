4cf5314142d6e718679decdc2b5c9518b87ad5c1
toxicmix <72023909+toxicmix@users.noreply.github.com>
2023-08-16 12:54:25 -0500

    possible missing closing bracket

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -37,42 +37,6 @@ router.get('/', async (req, res) => {
   });
   
 
-  // try {
-  //   const postData = await Post.findAll({
-  //     include: [User],
-  //   });
-
-  //   const posts = postData.map((post) => post.get({ plain: true }));
-
-  //   res.render('all-posts', { posts });
-  // } catch (err) {
-  //   res.status(500).json(err);
-  // }
 });
-
-// // get single post
-// router.get('/post/:id', async (req, res) => {
-//   try {
-//     const postData = await Post.findByPk(req.params.id, {
-//       include: [
-//         User,
-//         {
-//           model: Comment,
-//           include: [User],
-//         },
-//       ],
-//     });
-
-//     if (postData) {
-//       const post = postData.get({ plain: true });
-
-//       res.render('single-post', { post });
-//     } else {
-//       res.status(404).end();
-//     }
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
 
 module.exports = router;
