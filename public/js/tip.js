55fa0a6a743e9b1cdc54b7c1b41bf6244d787c0b
mengstrand <michael.d.engstrand1@gmail.com>
2023-08-16 12:55:03 -0500

    save before merge

---
+++ public/js/tip.js
@@ -0,0 +1,16 @@
+// const tipFormHandler = async function(event) {
+//   event.preventDefault();
+
+//   const topic = document.querySelector("#topic").value.trim();
+//   const content = document.querySelector("#tip-content").value.trim();
+  
+//   if (topic && content){
+//     const response = await fetch(`/api/tips`, (
+//       method: "POST",
+//       body: JSON.stringify({
+//         topic, 
+//         content,
+//       }),
+//     ))
+//   }
+}
