feb877b3e228bb59a21b3202cbba075929e74823
Chris_Gruen <113554860+ChristyGruen@users.noreply.github.com>
2023-08-17 11:41:23 -0500

    save before pull to main w Jacqueline updates

--- controllers/answer.controller.js
+++ controllers/answer.controller.js
@@ -1,4 +1,4 @@
-const { Answer } = require("../../models");
+const { Answer } = require("../models");
 
 async function addNewAnswers(answers, idOfQuestion) {
   const arrOfAnswers = answers.map({ ...answers, questionId: idOfQuestion });
