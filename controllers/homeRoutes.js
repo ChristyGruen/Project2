0dbd717f99a65a632f654d8be2b806eb56e32a8e
Chris_Gruen <113554860+ChristyGruen@users.noreply.github.com>
2023-08-17 09:47:16 -0500

    save before pull from main

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -36,48 +36,82 @@ router.get("/", async (req, res) => {
 
 });
 
-// // get single tip
-router.get("/tip/:id", async (req, res) => {
+// // // get single tip
+// router.get("/tip/:id", async (req, res) => {
+//   try {
+//     const tipData = await Tip.findByPk(req.params.id, {
+//       include: [{
+//         model: User,
+//         attributes: ['userName'],
+//       },],
+//     });
+
+//     const oneTip = tipData.get({ plain: true });
+//     console.log(oneTip)
+
+//     res.render('homepage', { oneTip });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+
+
+// // // get single Question
+// router.get("/question/:id", async (req, res) => {
+//   try {
+//     const questionData = await Question.findByPk(req.params.id, {
+//       include: [
+//         {
+//           model: User,
+//           attributes: ["userName"],
+//         },
+//         { model: Answer, attributes: ['content'] },
+//       ],
+//     });
+
+//     const oneQuestion = questionData.get({ plain: true });
+//     console.log(oneQuestion)
+
+//     res.render('homepage', { oneQuestion });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+router.get('/vote', async (req, res) => {
+  let tips, questions;
+
   try {
-    const tipData = await Tip.findByPk(req.params.id, {
-      include: [{
-        model: User,
-        attributes: ['userName'],
-      },],
-    });
-
-    const oneTip = tipData.get({ plain: true });
-    console.log(oneTip)
-
-    res.render('homepage', { oneTip });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-
-
-// // get single Question
-router.get("/question/:id", async (req, res) => {
-  try {
-    const questionData = await Question.findByPk(req.params.id, {
+    const tipData = await Tip.findAll({
       include: [
         {
           model: User,
           attributes: ["userName"],
         },
-        { model: Answer, attributes: ['content'] },
       ],
     });
-
-    const oneQuestion = questionData.get({ plain: true });
-    console.log(oneQuestion)
-
-    res.render('homepage', { oneQuestion });
+    tips = tipData.map((tippy) => tippy.get({ plain: true }));
   } catch (err) {
     res.status(500).json(err);
   }
+
+  try {
+    const questionData = await Question.findAll({
+      include: [
+        { model: User },
+        {
+          model: Answer,
+          attributes: ['content'],
+        },
+      ],
+    });
+    questions = questionData.map((questiony) => questiony.get({ plain: true }));
+  } catch (err) { res.status(500).json(err) }
+
+  res.render('vote', { tips, questions });
 });
+
+
 
 router.get('/login', (req, res) => {
   if (req.session.loggedIn) {
