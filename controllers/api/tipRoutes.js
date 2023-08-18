5193b41925acd8d1a4cb617ba3f18723adc1743a
mengstrand <michael.d.engstrand1@gmail.com>
2023-08-18 09:50:47 -0500

    save updates to tips - now able to post

--- controllers/api/tipRoutes.js
+++ controllers/api/tipRoutes.js
@@ -3,65 +3,34 @@ const { Tip, User } = require("../../models");
 const { addNewTip } = require("../tip.controller");
 // const withAuth = require("../../utils/auth");
 
-
-////////////////////////////move to homeRoutes.js///////////////////
-// router.get("/", // withAuth,
-//   async (req, res) => {
-//     try {
-//       const tipData = await Tip.findAll({
-//         include: [
-//           {
-//             model: User,
-//             // do we need to udpate all cases of userName to username
-//             attirbutes: ["userName"],
-//           },
-//         ],
-//       });
-
-//       const tips = tipData.map((tip) =>
-//         tip.get({ plain: true })
-//       );
-//       // katys note: home routes should use res.render(), api rout should use res.json()
-//       res.render("tips", {
-//         tips,
-//         // logged_in: req.session.logged_in,
-//       });
-//     } catch (err) {
-//       res.status(500).json(err);
-//     }
-//   }
-// );
-////////////////////////////move to homeRoutes.js///////////////////
-
-
-
-router.post("/", // withAuth,
-  async (req, res) => {
-    console.log("yoyo")
+router.post("/", async (req, res) => {
+console.log(req.body)
     try {
-
       const newTip = await Tip.create({
         ...req.body,
-                    // should the second userid be user_id
-        userid: req.session?.userid || 1,
+        userId: req.session.userId
       });
 
-      res.status(200).json(newTip);
+
+
+
+      res.status(200).json({ status: "success" });
     } catch (err) {
+      console.log(err);
       res.status(400).json(err);
     }
   });
 
 
 //////////////taken from homepage single question get///////////////
-router.post("/tip/", async (req, res) => {
+router.post("/tips", async (req, res) => {
   try {
     const tipData = await Tip.Create({
       include: [{
         model: User,
         attributes: ['userName'],
       },
-      { model: Tip, attirbutes: ["topic", "content"]}
+      { model: Tip, attirbutes: ["content", "title"]}
     ],
     });
 
@@ -74,46 +43,59 @@ router.post("/tip/", async (req, res) => {
     res.status(500).json(err);
   }
 });
-////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////
 
-// router.post("/", // withAuth,
-//   async (req, res) => {
-//     try {
-//       const newTip = await Tip.create({
-//         ...req.body,
-//         user_id: req.session.user_id,
-//       });
-
-//       res.status(200).json(newTip);
-//     } catch (err) {
-//       res.status(400).json(err);
-//     }
-//   });
-
-
-
-
-
-router.delete("/:id", 
-// withAuth,
-async (req, res) => {
-  try {
-    const tipData = await Tip.destroy({
-      where: {
-        id: req.params.id,
+router.post("/", // withAuth,
+  async (req, res) => {
+    try {
+      const newTip = await Tip.create({
+        ...req.body,
         user_id: req.session.user_id,
-      },
-    });
+      });
 
-    if (!tipData) {
-      res.status(404).json({ message: "No tip found with this id!" })
-      return;
+      res.status(200).json(newTip);
+    } catch (err) {
+      res.status(400).json(err);
     }
-    res.status(200).json(tipData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+  });
+
+
+
+
+
+// router.delete("/:id", 
+// // withAuth,
+// async (req, res) => {
+//   try {
+//     const tipData = await Tip.destroy({
+//       where: {
+//         id: req.params.id,
+//         user_id: req.session.user_id,
+//       },
+//     });
+
+//     if (!tipData) {
+//       res.status(404).json({ message: "No tip found with this id!" })
+//       return;
+//     }
+//     res.status(200).json(tipData);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 module.exports = router;
 
